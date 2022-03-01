7767516292eb13d8e9485e36aeda15e3735412ee
LorenzoGallo97 <92406178+LorenzoGallo97@users.noreply.github.com>
2022-03-01 19:42:11 -0300

    Add files via upload

---
+++ Json/main.js
@@ -0,0 +1,37 @@
+<><h1>Formularios Gallo</h1><h1>Bienvenidos a Formulario Gallo</h1><h1>Muestra de nuestro Stock</h1><div class="inicioGaleria">
+    <div class="inicio_formularios">
+        <img src="./Img/Formularios/02.jpg" alt="">
+        </></div>
+    <div class="_inicio_piedeFoto">
+        <p>A02</p>
+        <p>$575 (Gestor)</p>
+        <p>$670 (Particular)</p>
+    </div>
+</div><div class="inicioGaleria">
+        <div class="inicio_formularios">
+            <img src="./Img/Formularios/04.jpg" alt="">
+            </></div>
+        <div class="inicio_piedeFoto">
+            <p>A04</p>
+            <p>$575 (Gestor)</p>
+            <p>$670 (Particular)</p>
+        </div>
+    </div><div class="inicioGaleria">
+        <div class="inicio_formularios">
+            <img src="./Img/Formularios/M02.jpg" alt="">
+            </></div>
+        <div class="inicio_piedeFoto">
+            <p>M02</p>
+            <p>$330 (Gestor)</p>
+            <p>$485 (Particular)</p>
+        </div>
+    </div><div class="inicioGaleria">
+        <div class="inicio_formularios">
+            <img src="./Img/Formularios/M04.jpg" alt="">
+            </></div>
+        <div class="inicio_piedeFoto">
+            <p>M04</p>
+            <p>$330 (Gestor)</p>
+            <p>$485 (Particular)</p>
+        </div>
+    </div></>
